50c4ddd5571d121f84d1766d2f7c75c08e24f7cd
Eleonora Mattera <m.eleonora98@live.it>
2023-09-05 22:48:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import CustomHeader from "./components/CustomHeader/CustomHeader";
+
+function App() {
+  return (
+    <div className="App">
+      <CustomHeader />
+    </div>
+  );
+}
+
+export default App;
