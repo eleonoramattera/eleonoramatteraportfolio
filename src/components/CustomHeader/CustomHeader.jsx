34f09a046de102ce181c23e9b6c81f417b8e940b
Eleonora Mattera <m.eleonora98@live.it>
2023-09-06 23:59:01 +0200

    fix CustomHeader

--- src/components/CustomHeader/CustomHeader.jsx
+++ src/components/CustomHeader/CustomHeader.jsx
@@ -1,25 +1,53 @@
 import { Row, Col } from "react-bootstrap";
+import { gsap } from "gsap";
+import { useEffect, useState } from "react";
 import eleonora from "../../assets/eleonora.png";
+import margherita from "../../assets/margherita.svg";
+import CustomNav from "../CustomNav/CustomNav";
+import Jobs from "../Jobs/Jobs";
 import "./CustomHeader.css";
 
 function CustomHeader() {
+  const [showAlternateContent, setShowAlternateContent] = useState(false);
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    tl.to(".margherita", { rotation: 360, duration: 2 });
+
+    // Pausa di 1 secondo
+    tl.to({}, { duration: 1 });
+
+    // Animazione di h2 e img verso l'alto
+    tl.to("h2, .margherita", {
+      opacity: 0,
+      y: -100,
+      duration: 1,
+      ease: "power1.in",
+      onComplete: () => {
+        // Quando l'animazione è completata, mostra il contenuto alternativo
+        setShowAlternateContent(true);
+
+        tl.from("h1, h4", { opacity: 0, x: -50, duration: 2, stagger: 0.2 });
+      },
+    });
+  }, []);
+
   return (
     <>
-      <Row className="justify-center align-items-center">
-        <Col xs={12}>
-          <h1>WELCOME</h1>
-        </Col>
-      </Row>
-
-      <Row className="row-customHeader">
-        <Col xs={8}>
-          <h2>JUNIOR FULL-STACK DEVELOPER</h2>
-          <h2>GRAPHIC DESIGNER</h2>
-        </Col>
-        <Col xs={4}>
-          <img src={eleonora} alt="immagine personale" width="100%" />
-        </Col>
-      </Row>
+      {showAlternateContent ? (
+        <>
+          <CustomNav animateNav={showAlternateContent} />
+          <Jobs animateJobs={showAlternateContent} />
+        </>
+      ) : (
+        <Row className="row-customRow">
+          <Col xs={12} className="d-flex">
+            <h2>WELCOME</h2>
+            <img className="margherita" src={margherita} alt={margherita} width={"50em"} />
+          </Col>
+        </Row>
+      )}
     </>
   );
 }
