09f65becb610b676d7c0aa33e26478e3d22cb9fe
Eleonora Mattera <m.eleonora98@live.it>
2023-09-07 22:58:08 +0200

    Add Training

---
+++ src/components/Training/Training.jsx
@@ -0,0 +1,14 @@
+import { Row, Col } from "react-bootstrap";
+function Training() {
+  return (
+    <Row>
+      <Col>
+        <p>Epicode</p>
+        <p>PC Academy</p>
+        <p>Laurea in Comunicazione</p>
+      </Col>
+    </Row>
+  );
+}
+
+export default Training;
