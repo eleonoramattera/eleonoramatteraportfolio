25cffa072af03d738731873a55d4456900526fbd
Eleonora Mattera <m.eleonora98@live.it>
2023-09-10 20:23:00 +0200

    Update Project

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,11 +1,45 @@
 import { Row, Col } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Modal from "react-bootstrap/Modal";
+import polyhealth from "../../assets/polyhealth.png";
 import "./Project.css";
 function Project() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Row className="row-mainRow mt-5">
+    <Row className="row-customRow mt-5">
       <h3>PROJECTS</h3>
 
-      <Col></Col>
+      <Col xs={12}>
+        <Card style={{ width: "18rem" }} onClick={handleShow}>
+          <Card.Img variant="top" src={polyhealth} />
+          <Card.Body>
+            <Card.Title>POLYHEALTH</Card.Title>
+            <Card.Text>Final Capstone Project with Epicode</Card.Text>
+            <Button className="btn_card">View Code</Button>
+            <Button className="btn_card">View Demo</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>POLYHEALTH </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img src={polyhealth} alt="poject cover" />
+          React, Redux, PostrgeSQL, SpringBoot, JWT. Description Capstone Project di una clinica PolyHealth. Lo scopo è
+          semplificare la prenotazione di terapie. Gli utenti possono scegliere tra molteplici terapie e molteplici sedi
+          e prenotare
+          <Button className="btn_card">View Code</Button>
+          <Button className="btn_card">View Demo</Button>
+        </Modal.Body>
+      </Modal>
     </Row>
   );
 }
