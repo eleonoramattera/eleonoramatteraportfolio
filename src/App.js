886b8315c172a6779bca3217e06a2de1375eb114
Eleonora Mattera <m.eleonora98@live.it>
2023-09-11 22:07:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import CustomHeader from "./components/CustomHeader/CustomHeader";
 
