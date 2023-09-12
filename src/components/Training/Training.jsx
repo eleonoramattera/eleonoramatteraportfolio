670b8cd2958d5baa075913e5055b3e1ea5ceca22
Eleonora Mattera <m.eleonora98@live.it>
2023-09-12 09:54:33 +0200

    update Training

--- src/components/Training/Training.jsx
+++ src/components/Training/Training.jsx
@@ -16,14 +16,14 @@ function Training({ animateTraining }) {
       gsap.from(".training", {
         duration: 4,
         x: -100,
-        scrollTrigger: { trigger: "body", scrub: 2, markers: false },
+        scrollTrigger: { trigger: ".App", scrub: 2, markers: false },
       });
     }
   }, [animateTraining]);
 
   return (
-    <Row className="row-simpleLargeRow mt-5  training px-4">
-      <h3 className="position-relative" style={{ maxWidth: "20%" }}>
+    <Row className="row-mainRow mt-5  training px-4">
+      <h3 className="position-relative">
         TRAINING<span class="bottom"></span>
       </h3>
       <Col xs={12} className="d-flex flex-column my-4 ">
@@ -37,9 +37,14 @@ function Training({ animateTraining }) {
                 Full-stack Developer <br /> 2022-2023
               </h5>
               <p>
+                <span>Descrizione:</span> <br />
                 Corso intensivo full-stack con un approccio che combina teoria e pratica, mi ha permesso di applicare le
                 conoscenze apprese attraverso progetti reali, sia individualmente che in collaborazione con altri,
                 consentendmi di acquisire esperienza diretta in ogni aspetto dello sviluppo web full-stack.
+              </p>
+              <p>
+                <span> Skills:</span> <br />
+                Bootstrap | Sass | Javascript | Typescript | React | Redux | Java | Spring | Jwt | JUnit | PostgreSQL
               </p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -53,8 +58,14 @@ function Training({ animateTraining }) {
                 Master in Grafica Editoriale, Web Design & eCommerce <br /> 2020-2021
               </h5>
               <p>
+                <span>Descrizione:</span> <br />
                 Corso multidisciplinare che mi ha permesso di coniugare le competenze di grafica e web design offrendomi
-                una formazione completa che abbraccia entrambi questi campi creativi e complementari.
+                una formazione completa che abbraccia entrambi questi campi creativi e li integra in maniera
+                complementare.
+              </p>
+              <p>
+                <span> Skills:</span> <br />
+                HTML5 | CSS3 | WordPress | WooCommerce | Divi | Photoshop | InDesign | Illustrator | Ligthroom
               </p>
             </Carousel.Caption>
           </Carousel.Item>
@@ -68,9 +79,13 @@ function Training({ animateTraining }) {
                 Laurea triennale in Culture Digitali e della Comunicazione <br /> 2016-2020
               </h5>
               <p>
-                Percorso di studi che ha rappresentato un solido punto di partenza per la mia carriera nel web design e
-                in campi correlati. Durante il corso di laurea, ho acquisito competenze e conoscenze che sono rilevanti
-                per il web design e la comunicazione digitale.
+                Il mio percorso di laurea è stato un solido punto di partenza per le mie ambizioni professionali nel
+                mondo digitale. . Durante gli studi, ho acquisito competenze cruciali nella comunicazione digitale, il
+                marketing online, la gestione dei contenuti web e i principi di base dell'informatica e del web design.{" "}
+                <br />
+                Gli esami più significativi includono "Elementi di informatica e web", che ha coperto programmazione,
+                linguaggi di markup e networking, e "Web design e nuovi media", che si è concentrato sulla progettazione
+                di siti web e l'uso di nuovi media digitali.
               </p>
             </Carousel.Caption>
           </Carousel.Item>
